82c8b83971471b28c74bba98308f0d152ebb021f
jainnishant1 <44097820+jainnishant1@users.noreply.github.com>
2019-12-28 23:54:01 -0900

    Update app.js

--- loginRegister/app.js
+++ loginRegister/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 const db = require('./config/db');
 
 const app = express();
@@ -10,10 +12,25 @@ mongoose.connect(db.url, { useUnifiedTopology: true }, (err) => {
   console.log('Database Connected!!')
 })
 
+
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+})
 
 app.use('/', require('./routes/root'))
 
